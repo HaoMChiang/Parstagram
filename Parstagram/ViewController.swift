9cca65798ef460cd577c3320327acb38b0771152
Hao Chiang <haomingchiang@gmail.com>
2021-04-28 17:49:38 -0700

    Initial Commit

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by Hao-Ming Chiang on 4/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
